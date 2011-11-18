7f7b6e4243b1b5b95231f3879965868598bd6641
mikelietz <codger+git@gmail.com>
2011-11-18 14:24:01 +0000

    plugin:FreeStyleAdmin Update for trunk, including valid XML.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/FreeStyleAdmin/trunk@4060 f4a90942-1786-4aa1-b092-57c31935ddc3

--- freestyleadmin.plugin.php
+++ freestyleadmin.plugin.php
@@ -1,50 +1,26 @@
 <?php
 class FreeStyleAdmin extends Plugin {
-    function info() {
-        return array(
-            'url'            => 'http://iamgraham.net/plugins',
-            'name'            => 'FreeStyleAdmin',
-            'description'    => 'Allows you to load arbitrary CSS files into the admin pages.',
-            'license'        => 'Apache License 2.0',
-            'author'        => 'Graham Christensen',
-            'authorurl'        => 'http://iamgraham.net/',
-            'version'        => '0.1'
-        );
-    }
 
-    public function filter_plugin_config($actions, $plugin_id) {
-        if ($plugin_id == $this->plugin_id()) {
-            $actions[] = _t('Configure');
-        }
-        return $actions;
-    }
+	public function configure() {
 
-    public function action_plugin_ui($plugin_id, $action) {
+		$ui = new FormUI(strtolower(get_class($this)));
+		$css_location = $ui->append('text', 'css_location', 'option:freestyleadmin__css_location', _t('FreeStyle CSS Location', 'FreeStyleAdmin'));
+		$css_location->add_validator( 'validate_required' );
 
-        if ($plugin_id == $this->plugin_id()) {
-            switch ($action) {
-                case _t('Configure'):
-                    $ui = new FormUI(strtolower(get_class($this)));
-                    $css_location = $ui->append('text', 'css_location', 'option:freestyleadmin__css_location', _t('FreeStyle CSS Location', 'FreeStyleAdmin'));
-                    $css_location->add_validator( 'validate_required' );
-                    
-                    $ui->append( 'submit', 'save', _t( 'Save', 'FreeStyleAdmin' ) );
-                    $ui->set_option( 'success_message', _t( 'Configuration saved', 'FreeStyleAdmin' ) );
-                    $ui->out();
-                break;
-            }
-        }
-    }
+		$ui->append( 'submit', 'save', _t( 'Save', 'FreeStyleAdmin' ) );
+		$ui->set_option( 'success_message', _t( 'Configuration saved', 'FreeStyleAdmin' ) );
+		return $ui;
+	}
 
-    private static function getvar($var) {
-        return Options::get('freestyleadmin__'.$var);
-    }
+	private static function getvar($var) {
+		return Options::get('freestyleadmin__'.$var);
+	}
 
-    function action_admin_header() {
+	function action_admin_header() {
 		if (self::getvar('css_location') != '') {
 			Stack::add('admin_stylesheet', array(self::getvar('css_location'), 'screen'));
 		}
-    }
+	}
 
 }
 ?>
